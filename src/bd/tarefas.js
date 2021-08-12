26289d4dace10d09678725f45569cf48a6edee88
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2021-08-11 21:36:31 -0300

    Criando um modelo base para uma interface com o banco de dados

--- src/bd/tarefas.js
+++ src/bd/tarefas.js
@@ -1,1 +1,20 @@
-/* CRUD para as tarefas */
+const tabelas = require('./schema')
+const Tarefa = tabelas.Tarefa
+
+function criarTarefa(tarefa) {
+
+    const tarefa = Tarefa.create({
+        titulo: "Um titulo",
+    })
+
+    let id = tarefa.id;
+
+    console.log(`O id é: ${id}`)
+    return id;
+}
+
+function recuperListaDeTarefas() {
+    let tarefas = Tarefa.findAll()
+
+    return tarefas;
+}
