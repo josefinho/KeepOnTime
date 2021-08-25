1e66d78547651785333c7609e969a3a47e32c0b4
Maikon <maikonrosa2010@hotmail.com>
2021-08-25 19:27:04 -0300

    Adicionando opção de checkar todos os checkboxes

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,31 @@
-function redirect() {
-    window.location.href = window.location.hostname;
+function getAllCheckInputs() {
+    let inputs = [];
+    let checkboxes = [];
+
+    inputs = document.getElementsByTagName("input");
+    for(let i = 0; i < inputs.length; i++) {
+        if(inputs[i].getAttribute("type") === "checkbox")
+            checkboxes.push(inputs[i]);
+    }
+
+    return checkboxes;
 }
 
-function selection_global(){
+function selectAllChecks(){
+    let checkboxes = getAllCheckInputs();
 
+    document.getElementById("globalcheck").checked = false;
+
+    for(let i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked === true) {
+            for(let j = 0; j < checkboxes.length; j++) {
+                checkboxes[j].checked = false;
+            }
+            return;
+        }
+    }
+
+        for(let j = 0; j < checkboxes.length; j++) {
+            checkboxes[j].checked = true;
+        }
 }
