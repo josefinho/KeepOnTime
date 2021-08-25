8a68af43c6315cc6bfda45370a689b9bf37b0a5c
tgmm2712 <tiagomalaguti@alunos.utfpr.edu.br>
2021-08-24 21:20:58 -0300

    24/08/2021 restruturacao projeto

--- src/bd/create_db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './tarefas.sqlite',
-})
-
-module.exports = sequelize
--- test/index.js
+++
@@ -1,1 +0,0 @@
-// TODO: Decidir framework de testes.
