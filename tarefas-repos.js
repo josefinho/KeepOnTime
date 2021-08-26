379766d86a1356c7e27be11cb1e5a0e7f1cbd152
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2021-08-26 10:25:48 -0300

    Adicionando funções para maniular o banco de dados

--- tarefas-repos.js
+++ tarefas-repos.js
@@ -11,12 +11,13 @@ function fecharConexao() {}
  * 
  * Exemplo de objeto de tarefa:
  * {
- *   id: 1,
  *   titulo: "teste",
  *   prazo: "1999-12-12",
  *   hora: "15:23",
  *   descricao: "testte"
  * };
+ * @param {Object} tarefa 
+ * @returns {uuid} id da tarefa criada
  */
 async function criarTarefa(tarefa) {
     const novaTarefa = await Tarefa.create({
@@ -29,7 +30,47 @@ async function criarTarefa(tarefa) {
     return novaTarefa.id;
 }
 
+/** 
+ * Manda o id da tarefa a ser editada e um objeto contendo
+ * os campos a serem atualizados.
+ * 
+ * @param {uuid} id_tarefa 
+ * @param {Object} tarefaEditada 
+ */
+async function editarTarefa(id_tarefa, tarefaEditada) {
+    const tarefa = await Tarefa.findByPk(id_tarefa);
+    Object.keys(tarefaEditada).forEach(function(coluna) {
+        if (tarefa.hasOwnProperty(coluna))
+            tarefa[coluna] = tarefaEditada[coluna];
+    });
+    tarefa.save();
+}
 
+/**
+ * Muda apenas o status de tarefa
+ * 
+ * @param {uuid} id_tarefa 
+ * @param {Boolean} status 
+ */
+async function mudarStatusTarefa(id_tarefa, status) {
+    const tarefa = await Tarefa.findByPk(id_tarefa);
+    tarefa.status = status;
+    tarefa.save();
+}
+
+/**
+ * @param {uuid} tarefa_id 
+ */
+async function removerTarefa(tarefa_id) {
+    const tarefa = await Tarefa.findByPk(tarefa_id);
+    tarefa.destroy();
+}
+
+/**
+ * 
+ * @param {uuid} caderno_id 
+ * @returns {Object[]} Lista de tarefas
+ */
 async function pegarListaTarefas(caderno_id) {
     if (caderno_id)
         return await Tarefa.findAll({ where: {caderno_id: caderno_id}})
@@ -37,13 +78,10 @@ async function pegarListaTarefas(caderno_id) {
         return await Tarefa.findAll();
 }
 
-async function concluirTarefa(tarefa_id) {
-
-}
-
-
 module.exports = {
     criarTarefa,
     pegarListaTarefas,
-
+    editarTarefa,
+    removerTarefa,
+    mudarStatusTarefa,
 }
